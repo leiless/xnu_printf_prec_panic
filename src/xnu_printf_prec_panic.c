edbbf3972d7d6f22e8aa95400200342419ab1f85
Lei <lynnl.yet@gmail.com>
2019-01-26 20:44:28 +0800

    Remove kernel thread probe implementation
    Data in heap(_MALLOC, kern_os_malloc) cannot reproduce the panic

--- src/xnu_printf_prec_panic.c
+++ src/xnu_printf_prec_panic.c
@@ -8,109 +8,19 @@
 #include <kern/task.h>
 #include "xnu_printf_prec_panic.h"
 
-/**
- * Terminate a thread(not exported KPI)
- * @return  0 if terminated  errno o.w.
- *          KERN_INVALID_ARGUMENT   if thread is THREAD_NULL
- *          KERN_FAILURE            if not terminate by itself
- *          KERN_TERMINATED         if the thread already terminated
- * see: xnu/osfmk/kern/thread_act.c#thread_terminate
- */
-extern kern_return_t thread_terminate(thread_t);
-
-static thread_t panic_thd = THREAD_NULL;
-static volatile int8_t cond_keepalive = 1;
-
-/**
- * Do printf() precision print panic test
- */
-static void printf_prec_panic_test(void)
-{
-    int sz = 11;
-    char *p = _MALLOC(sz, M_TEMP, M_NOWAIT);
-    int n;
-
-    if (p == NULL) {
-        LOG_ERR("_MALLOC() fail  OOM");
-        return;
-    }
-
-    n = snprintf(p, sz, "%#010x", (uint32_t) p);
-    kassertf(n == sz-1, "n is %d", n);
-    kassertf(p[sz-1] == '\0', "p[%d] is %#x", sz-1, p[sz-1]);
-    /* Manually clear the EOS to test printf() precision print */
-    p[sz-1] = '!';
-
-    LOG_DBG("%.*s", sz-1, p);
-    _FREE(p, M_TEMP);
-}
-
-#define MS_PER_NS       1000000
-
-/*
- * XXX:
- *  if you intended to keep this thread running even if
- *  kernel extension is unloaded  you should never access any kext var/func
- *  instead all data should be allocated dynamically
- *  o.w. the kernel will panic(memory access violation)
- *
- *  if you intended to stop this thread when kext unloading
- *      and this thread access kext var/func
- *  you should make sure this thread stop before kext unload
- */
-static void thread_runloop(void *arg __unused, wait_result_t wres __unused)
-{
-    static struct timespec ts = {0, 10 * MS_PER_NS};
-
-    while (cond_keepalive) {
-        printf_prec_panic_test();
-        (void) msleep((void *) &cond_keepalive, NULL, PPAUSE, NULL, &ts);
-    }
-
-    kassert(current_thread() == panic_thd);
-    LOG("conditional variable changed");
-    LOG("kernel thread %p:%#llx is dying", panic_thd, thread_tid(panic_thd));
-
-    /* for the extra refcnt from kernel_thread_start() */
-    thread_deallocate(current_thread());
-
-    /*
-     * signal blocked kext unload procedure to continue
-     *  there still negligible chance between thread_terminate and kextunload
-     */
-    cond_keepalive = -1;
-
-    /* thread done */
-    (void) thread_terminate(current_thread());
-
-    /* see: xnu/bsd/net/dlil.c#ifnet_start_thread_fn */
-    __builtin_unreachable();
-}
-
 kern_return_t xnu_printf_prec_panic_start(
         kmod_info_t *ki __unused,
         void *d __unused)
 {
-    kern_return_t e;
-    e = kernel_thread_start(thread_runloop, NULL, &panic_thd);
-    if (e != KERN_SUCCESS) {
-        LOG_ERR("kernel_thread_start() fail  errno: %d", e);
-    } else {
-        LOG("kernel thread %p:%#llx started", panic_thd, thread_tid(panic_thd));
-    }
-    return e;
+
+    return KERN_SUCCESS;
 }
 
 kern_return_t xnu_printf_prec_panic_stop(
         kmod_info_t *ki __unused,
         void *d __unused)
 {
-    cond_keepalive = 0;
-    wakeup_one((void *) &cond_keepalive);
-    /* Busy wait the thread to finish */
-    while (cond_keepalive != -1) continue;
 
-    LOG("kext unloaded");
     return KERN_SUCCESS;
 }
 
