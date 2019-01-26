aa02309b690945796baee460f26ac9ce7ed90c99
Lei <lynnl.yet@gmail.com>
2019-01-26 16:46:14 +0800

    Secure thread_runloop() termination
    Since the thread shares code between the kext
    Thus we should terminate the thread before unload the kext
    o.w. the kernel will panic

--- src/xnu_printf_prec_panic.c
+++ src/xnu_printf_prec_panic.c
@@ -19,7 +19,7 @@
 extern kern_return_t thread_terminate(thread_t);
 
 static thread_t panic_thd = THREAD_NULL;
-static volatile uint8_t cond_keepalive = 1;
+static volatile int8_t cond_keepalive = 1;
 
 /**
  * Do printf() precision print panic test
@@ -35,12 +35,16 @@ static void printf_prec_panic_test(void)
  * XXX:
  *  if you intended to keep this thread running even if
  *  kernel extension is unloaded  you should never access any kext var/func
+ *  instead all data should be allocated dynamically
  *  o.w. the kernel will panic(memory access violation)
+ *
+ *  if you intended to stop this thread when kext unloading
+ *      and this thread access kext var/func
+ *  you should make sure this thread stop before kext unload
  */
 static void thread_runloop(void *arg __unused, wait_result_t wres __unused)
 {
     static struct timespec ts = {10, 0};
-    kern_return_t e;
 
     while (cond_keepalive) {
         printf_prec_panic_test();
@@ -53,9 +57,15 @@ static void thread_runloop(void *arg __unused, wait_result_t wres __unused)
 
     /* for the extra refcnt from kernel_thread_start() */
     thread_deallocate(current_thread());
+
+    /*
+     * signal blocked kext unload procedure to continue
+     *  there still negligible chance between thread_terminate and kextunload
+     */
+    cond_keepalive = -1;
+
     /* thread done */
-    e = thread_terminate(current_thread());
-    kassertf(e == KERN_SUCCESS, "thread_terminate() fail  errno: %d", e);
+    (void) thread_terminate(current_thread());
 
     /* see: xnu/bsd/net/dlil.c#ifnet_start_thread_fn */
     __builtin_unreachable();
@@ -81,6 +91,9 @@ kern_return_t xnu_printf_prec_panic_stop(
 {
     cond_keepalive = 0;
     wakeup_one((void *) &cond_keepalive);
+    /* Busy wait the thread to finish */
+    while (cond_keepalive != -1) continue;
+
     LOG("kext unloaded");
     return KERN_SUCCESS;
 }
