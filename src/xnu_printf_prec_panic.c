e681b2c707f9c4eec106b1b5a2e0ead95bf9c980
Lei <lynnl.yet@gmail.com>
2019-01-26 11:17:17 +0800

    Add initial generic kext code

---
+++ src/xnu_printf_prec_panic.c
@@ -0,0 +1,44 @@
+/*
+ * Created 190126 lynnl
+ */
+
+#include <mach/mach_types.h>
+
+#ifndef __TS__
+#define __TS__      "????/??/?? ??:??:??+????"
+#endif
+
+kern_return_t xnu_printf_prec_panic_start(
+        kmod_info_t *ki __unused,
+        void *d __unused)
+{
+
+    return KERN_SUCCESS;
+}
+
+kern_return_t xnu_printf_prec_panic_stop(
+        kmod_info_t *ki __unused,
+        void *d __unused)
+{
+
+    return KERN_SUCCESS;
+}
+
+#ifdef __kext_makefile__
+extern kern_return_t _start(kmod_info_t *, void *);
+extern kern_return_t _stop(kmod_info_t *, void *);
+
+/* Will expand name if it's a macro */
+#define KMOD_EXPLICIT_DECL2(name, ver, start, stop) \
+    __attribute__((visibility("default")))          \
+        KMOD_EXPLICIT_DECL(name, ver, start, stop)
+
+KMOD_EXPLICIT_DECL2(BUNDLEID, KEXTBUILD_S, _start, _stop)
+
+/* If you intended to write a kext library  NULLify _realmain and _antimain */
+__private_extern__ kmod_start_func_t *_realmain = xnu_printf_prec_panic_start;
+__private_extern__ kmod_stop_func_t *_antimain = xnu_printf_prec_panic_stop;
+
+__private_extern__ int _kext_apple_cc = __APPLE_CC__;
+#endif
+
